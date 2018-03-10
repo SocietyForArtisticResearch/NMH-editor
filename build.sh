1e3bec03f6149a5be10d79e641ce5b6595339efc
Casper Schipper <casper.schipper@gmail.com>
2018-03-10 07:22:43 -0600

    improved build script

--- build.sh
+++ build.sh
@@ -3,5 +3,8 @@
 # build the angular app, specify root path
 ng build --base-href /mnh-test/
 
-# copy to html folder
+# remove old build
+rm -r ~/public_html/mnh-test
+
+# copy new to html folder
 cp -r -v ./dist ~/public_html/mnh-test 
