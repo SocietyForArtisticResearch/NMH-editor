fe435831a5130e13b947395869b8fffd02a23959
Luc Döbereiner <luc@computer.localdomain>
2018-03-12 10:03:51 +0100

    httpS

--- src/app/shared/convert-doc.service.ts
+++ src/app/shared/convert-doc.service.ts
@@ -8,26 +8,26 @@ import * as FileSaver from 'file-saver';
 @Injectable()
 export class ConvertDocService {
 
-	constructor(private http: HttpClient) {
-	
-	}
+    constructor(private http: HttpClient) {
 
-    convert(markdownString: string, fileType : string) { //get file from service
-    		let url = "http://localhost:3000/export/"+fileType;
-    		let expositionJson = { markdown : markdownString };
-	        let headers = new HttpHeaders({'Content-Type' : 'application/json'});
-	      
+    }
 
-	        const options = {
-	        	headers, 
-	        	responseType: 'blob' as 'text'
-	        };
+    convert(markdownString: string, fileType: string) { //get file from service
+        let url = "https://localhost:3000/export/" + fileType;
+        let expositionJson = { markdown: markdownString };
+        let headers = new HttpHeaders({ 'Content-Type': 'application/json' });
 
-	        this.http.post(url , expositionJson, options).subscribe(
-	        (response: any) => { // download file
-	            console.log(response);
-	            let filename = 'file.'+fileType;
-	            FileSaver.saveAs(response, filename);
-	    });
-	}
+
+        const options = {
+            headers,
+            responseType: 'blob' as 'text'
+        };
+
+        this.http.post(url, expositionJson, options).subscribe(
+            (response: any) => { // download file
+                console.log(response);
+                let filename = 'file.' + fileType;
+                FileSaver.saveAs(response, filename);
+            });
+    }
 }
