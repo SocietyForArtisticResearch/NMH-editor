c9e1021194054c826f40172d6172570d662e0135
Casper Schipper <casper.schipper@gmail.com>
2018-03-15 10:51:25 +0100

    fixed some issues with markdown demo

--- src/app/tools/markdown-tool/markdown-tool.component.ts
+++ src/app/tools/markdown-tool/markdown-tool.component.ts
@@ -50,7 +50,9 @@ export class MarkdownToolComponent implements AfterViewInit {
     testString( ) {
         return `# An exhibit of Markdown
 
-This note demonstrates some of what [Markdown][^1] is capable of doing.
+This note demonstrates some of what [Markdown](http://daringfireball.net/projects/markdown/) is capable of doing.
+
+*Note: Feel free to play with this page. Unlike regular notes, this doesn't automatically save itself.*
 
 ## Basic formatting
 
@@ -67,7 +69,7 @@ Paragraphs must be separated by a blank line. Basic formatting of *italics* and 
 3. Number 3
 4. Ⅳ
 
-*Note: the fourth item uses the Unicode character for [Roman numeral four][2].*
+*Note: the fourth item uses the Unicode character for [Roman numeral four](http://www.fileformat.info/info/unicode/char/2163/index.htm).*
 
 ### Unordered list
 
@@ -75,6 +77,17 @@ Paragraphs must be separated by a blank line. Basic formatting of *italics* and 
 * Another item
 * Yet another item
 * And there's more...
+
+# Footnotes
+This is a footnote [^1]
+
+## Paragraph modifiers
+
+### Code block
+
+    Code blocks are very useful for developers and other people who look at code or other things that are written in plain text. As you can see, it uses a fixed-width font.
+
+You can also make \`inline code\` to add code into other things.
 
 ### Quote
 
@@ -84,23 +97,19 @@ Paragraphs must be separated by a blank line. Basic formatting of *italics* and 
 
 There are six levels of headings. They correspond with the six levels of HTML headings. You've probably noticed them already in the page. Each level down uses one more hash character.
 
-# Heading 1
-## Heading 2
-### Heading 3
-#### Heading 4
-##### Heading 5
-###### Heading 6
-
 ### Headings *can* also contain **formatting**
 
+### They can even contain \`inline code\`
+
 Of course, demonstrating what headings look like messes up the structure of the page.
+
 I don't recommend using more than three or four levels of headings here, because, when you're smallest heading isn't too small, and you're largest heading isn't too big, and you want each size up to look noticeably larger and more important, there there are only so many sizes that you can use.
 
 ## URLs
 
 URLs can be made in a handful of ways:
 
-* A named link to [MarkItDown][^3]. The easiest way to do these is to select what you want to make a link and hit 'Ctrl+L'.
+* A named link to [MarkItDown](http://www.markitdown.net/). The easiest way to do these is to select what you want to make a link and hit 'ctrl+K' & 'cmd+K'
 * Another named link to [MarkItDown](http://www.markitdown.net/)
 * Sometimes you just want a URL like <http://www.markitdown.net/>.
 
@@ -112,17 +121,16 @@ A horizontal rule is a line that goes across the middle of the page.
 
 It's sometimes handy for breaking things up.
 
+## Images
+
+Markdown can also contain images. I'll need to add something here sometime.
 
 ## Finally
 
-There's actually a lot more to Markdown than this. See the official [introduction][4] and [syntax][5] for more information. However, be aware that this is not using the official implementation, and this might work subtly differently in some of the little things.
+There's actually a lot more to Markdown than this. See the official [introduction](http://daringfireball.net/projects/markdown/basics) and [syntax](http://daringfireball.net/projects/markdown/syntax) for more information. However, be aware that this is not using the official implementation, and this might work subtly differently in some of the little things.
 
 
-  [^1]: http://daringfireball.net/projects/markdown/
-  [^2]: http://www.fileformat.info/info/unicode/char/2163/index.htm
-  [^3]: http://www.markitdown.net/
-  [^4]: http://daringfireball.net/projects/markdown/basics
-  [^5]: http://daringfireball.net/projects/markdown/syntax
+[^1]: The footnote text
 `;
     }
 
