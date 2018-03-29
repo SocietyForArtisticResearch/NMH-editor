9c23d3cd98b2061e5b170aead26c735fffa25964
Casper Schipper <casper.schipper@gmail.com>
2018-03-29 14:23:45 +0200

    added upload progress in dropbox

--- src/app/drag-and-drop/drag-and-drop.component.ts
+++ src/app/drag-and-drop/drag-and-drop.component.ts
@@ -3,7 +3,7 @@ import { DragDropDirective } from './drag-drop.directive';
 import { Backend } from '../shared/Backend';
 import { RCExpoModel } from '../shared/RCExpoModel';
 import { RCImage, RCObject, RCAudio, RCVideo } from '../shared/rcexposition';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpRequest, HttpEventType, HttpResponse  } from '@angular/common/http';
 //import { BlobMimeDetect } from '../shared/BlobMimeDetect';
 
 
@@ -16,51 +16,76 @@ import { HttpClient } from '@angular/common/http';
 export class DragAndDropComponent implements OnInit {
   @Output() onChangedObject = new EventEmitter();
 
-  constructor(private http: HttpClient, private rcExpoModel: RCExpoModel) { }
+  fileUploadStatus: string = null;
+
+  constructor(
+    private http: HttpClient, 
+    private rcExpoModel: RCExpoModel
+  ) { }
 
   ngOnInit() {
   
   }
 
   onFilesChange(fileList : FileList) {
+    this.fileUploadStatus = 'upload in progress';
     // image dropped (should add check !)
     if (fileList.length > 0) {
       
-      let selectedImage = fileList[0];
+      let selectedFile = fileList[0];
 
       const fd = new FormData();
-      fd.append('uploadFile', selectedImage, selectedImage.name);
-      this.http.post(Backend.uploadAddress, fd).subscribe(result => {
-        this.onResult(result);
+      fd.append('uploadFile', selectedFile, selectedFile.name);
+      // this.http.post(Backend.uploadAddress, fd).subscribe(result => {
+      //   this.onResult(result);
+      // });
+
+      const req = new HttpRequest('POST', Backend.uploadAddress, fd, {
+        reportProgress: true,
+      });
+
+      this.http.request(req).subscribe(event => {
+        // Via this API, you get access to the raw event stream.
+        // Look for upload progress events.
+        if (event.type === HttpEventType.UploadProgress) {
+          // This is an upload progress event. Compute and show the % done:
+          this.fileUploadStatus = Math.round(100 * event.loaded / event.total) + '%';
+          console.log('this.fileUploadStatus',this.fileUploadStatus)
+        } else if (event instanceof HttpResponse) {
+          this.fileUploadStatus = 'done';
+          window.setTimeout( ( ) => { this.fileUploadStatus = null }, 1000 );
+          this.onResult(event.body);
+          }
       });
     }
   }
 
   onResult(result) {
       let mimeType = result.mime;
+
+      let newRCObject: RCObject = null;
       
       if (mimeType.includes('image')) {
 
         let imageName = 'image' + this.rcExpoModel.exposition.media.length;
-        
-
         let imageUri = Backend.baseAddress + result.url;
-        let imageObject = new RCImage(imageName, imageUri, 'myClass', null, null);
-
-
-        this.rcExpoModel.exposition.addObject(imageObject);
-
-        this.onChangedObject.emit(imageObject.id);
+        newRCObject = new RCImage(imageName, imageUri, 'myClass', null, null);
       } else if (mimeType.includes('audio')) {
         let audioName = 'audio' + this.rcExpoModel.exposition.media.length;
-
         let audioUri = Backend.baseAddress + result.url;
         //(name: string, url: string, autoplay, loop, userClass, pxWidth?: number, pxHeight?: number) {
         // super(name, url, "rcaudio", userClass, pxWidth, pxHeight);
-        let audioObject = new RCAudio(audioName,audioUri, false, false, 'myClass', null,null);
-        this.rcExpoModel.exposition.addObject(audioObject);
-        this.onChangedObject.emit(audioObject.id);
+        newRCObject = new RCAudio(audioName,audioUri, false, false, 'myClass', null,null);
+   
+      } else if (mimeType.includes('video')) {
+        let videoName = 'video' + this.rcExpoModel.exposition.media.length;
+        let videoUri = Backend.baseAddress + result.url;
+        //(name, url, autoplay, loop, userClass, pxWidth?: number, pxHeight?: number) 
+        newRCObject = new RCVideo(videoName,videoUri,'myClass',null,null);
       }
+
+      this.rcExpoModel.exposition.addObject(newRCObject);
+      this.onChangedObject.emit(newRCObject.id);
   }
 
   onClick() {
