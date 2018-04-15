032be1d17d83bf7ce96df31cddbfb5c3def1d14d
Luc Döbereiner <luc@computer.localdomain>
2018-04-15 15:38:41 +0200

    build script base-href2

--- build-prod.sh
+++ build-prod.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 # build the angular app, specify root path
-ng build --base-href /text-editor --prod
+ng build --base-href /texteditor --prod
 
 # remove old build
 # rm -r ~/public-dev/mnh-test
 
 # copy new to html folder
 # cp -r -v ./dist ~/public-dev/mnh-test 
+rm -rf ../public-dev/*
 
+cp -a dist/. ../public-dev/
