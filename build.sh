41edd0343e538a5168d10c929c197978445a910e
Luc Döbereiner <luc@computer.localdomain>
2018-04-16 12:11:18 +0200

    build

--- build.sh
+++ build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 # build the angular app, specify root path
-ng build --base-href /mnh-test/
+ng build --base-href /texteditor 
 
 # remove old build
-rm -r ~/public_html/mnh-test
+# rm -r ~/public-dev/mnh-test
 
 # copy new to html folder
-cp -r -v ./dist ~/public_html/mnh-test 
+# cp -r -v ./dist ~/public-dev/mnh-test 
+rm -rf ../public-dev/*
+
+cp -a dist/. ../public-dev/
