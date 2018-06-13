d5a6ec2014eb65af5c6ca60f601e0d4e98fa668f
Luc Döbereiner <luc@computer.localdomain>
2018-06-13 13:09:06 +0200

    url fix

--- src/app/shared/Backend.ts
+++ src/app/shared/Backend.ts
@@ -9,13 +9,14 @@ export class Backend {
     static import: string = "https://sar-announcements.com:3000/import";
     static sarExport: string = "https://sar-announcements.com:3000/export";
 
-    static rcMediaUpload: string = "/text-editor/simple-media-add";
-    static rcMediaRemove: string = "/text-editor/simple-media-remove";
-    static rcMediaEdit: string = "/text-editor/simple-media-edit";
-    static rcBaseAddress: string = location.hostname;
-    static rcMediaList: string = "/text-editor/simple-media-list";
-    static rcImport: string = "/text-editor/import";
-    static rcExport: string = "/text-editor/export";
+
+    static rcMediaUpload: string = "//" + location.hostname + "/text-editor/simple-media-add";
+    static rcMediaRemove: string = "//" + location.hostname + "/text-editor/simple-media-remove";
+    static rcMediaEdit: string = "//" + location.hostname + "/text-editor/simple-media-edit";
+    static rcBaseAddress: string = "//" + location.hostname;
+    static rcMediaList: string = "//" + location.hostname + "/text-editor/simple-media-list";
+    static rcImport: string = "//" + location.hostname + "/text-editor/import";
+    static rcExport: string = "//" + location.hostname + "/text-editor/export";
 
 
     // set if RC is to be used as backend
